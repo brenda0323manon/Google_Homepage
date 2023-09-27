0287e2097fcb41a1f4b95e32fa302f037465cf2a
Brenda <Brenda_Manon@Administrators-MacBook-Pro.local>
2023-09-27 11:05:04 -0400

    trying to push

---
+++ page.js
@@ -0,0 +1,14 @@
+ function alertUser() {
+    let search = document.getElementById('#searchBar').value;
+    if (search) {
+        alert("https://www.google.com/search" + search);
+    }
+    
+}
+
+// function Alert() {
+//        let searchQuery = document.querySelectorAll('#searchBar').value;
+// }
+     
+
+
